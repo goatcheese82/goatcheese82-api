a16e7d57d4e92556801a77c178c640e39372096f
goatcheese82 <nonsquishy@gmail.com>
2019-03-01 12:34:38 -0600

    added user resource

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+    def login
+        auth_object = Authentication.new(login_params)
+        if auth_object.authenticate
+            render json: {
+                message: "login successful!",
+                token: auth_object.generate_token },
+            status: :ok
+        else
+            render json: {
+                message: "incorrect username/password combination"},
+                status: :unauthorized
+        end
+    end
+
+    private
+
+    def login_params
+        params.permit(:username, :password)
+    end
 end
