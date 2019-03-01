2fafa00799f37340641a4546242bfad085bf53ae
goatcheese82 <nonsquishy@gmail.com>
2019-03-01 13:08:00 -0600

    added index, new, and create to users controller, added all attributes to user serializer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,9 +13,54 @@ class UsersController < ApplicationController
         end
     end
 
+
+    def index
+        @users = User.all
+        render json: @users
+    end
+
+    def new
+        @user = User.new
+    end
+    def create
+        @user = User.create(user_params)
+        if @user.save
+            render json: @user
+        else
+            render json: {
+                message: "Please fill all fields with valid"
+            }
+        end
+    end
+
+    def show
+        @user = User.find(params[:id])
+        render json: @user
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
     private
 
     def login_params
         params.permit(:username, :password)
+    end
+
+    def user_params
+        params.permit(
+            :username,
+            :password,
+            :name,
+            :password,
+            :email,
+            :uid
+        )
     end
 end
